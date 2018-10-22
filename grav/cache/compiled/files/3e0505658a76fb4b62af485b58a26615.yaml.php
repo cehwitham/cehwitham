ff168ad05fe1d0980872bfbbf9a5b442b4cb6ec8
cehwitham <cehwitham@gmail.com>
2018-10-22 22:54:26 +0100

    Rearrange directory for GitHub user pages setup

---
+++ grav/cache/compiled/files/3e0505658a76fb4b62af485b58a26615.yaml.php
@@ -0,0 +1,29 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Users/cehwitham/Sites/cehwitham.github.io/grav/system/config/streams.yaml',
+    'modified' => 1539002858,
+    'data' => [
+        'schemes' => [
+            'image' => [
+                'type' => 'ReadOnlyStream',
+                'paths' => [
+                    0 => 'user://images',
+                    1 => 'system://images'
+                ]
+            ],
+            'page' => [
+                'type' => 'ReadOnlyStream',
+                'paths' => [
+                    0 => 'user://pages'
+                ]
+            ],
+            'account' => [
+                'type' => 'ReadOnlyStream',
+                'paths' => [
+                    0 => 'user://accounts'
+                ]
+            ]
+        ]
+    ]
+];
---
+++ grav/cache/compiled/files/73ea15ef43a9f3d03ed9c85f73a3fd23.yaml.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Users/cehwitham/Sites/cehwitham.github.io/grav/user/config/streams.yaml',
+    'modified' => 1540244135,
+    'data' => [
+        
+    ]
+];
